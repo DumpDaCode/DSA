f351a38267ce879e3103bddaa84cfa4f49ef9b19
Rajivranjan Singh <rajivranjans@mkcl.org>
2022-08-31 16:11:46 +0530

    updates to counting sort test

--- sorting/linear/countingSort_test.go
+++ sorting/linear/countingSort_test.go
@@ -6,13 +6,23 @@ import (
 )
 
 func TestCountingSort(t *testing.T) {
-	t.Run("Basic Input", func(t *testing.T) {
-		arr := []int{0, 0, 1, 1, 0, 0, 1, 0}
-		want := []int{0, 0, 0, 0, 0, 1, 1, 1}
-		got := CountingSort(arr, 3)
-		t.Log(got)
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("Got: %v, Want: %v", got, want)
+	var testCases = []struct {
+		name          string
+		arr           []int
+		want          []int
+		intergerRange []int
+	}{
+		{
+			name:          "Array containing numbers from range(0-1)",
+			arr:           []int{0, 0, 1, 1, 0, 0, 1, 0},
+			want:          []int{0, 0, 0, 0, 0, 1, 1, 1},
+			intergerRange: []int{0, 1},
+		},
+	}
+	for _, testCase := range testCases {
+		got := CountingSort(testCase.arr, testCase.intergerRange)
+		if !reflect.DeepEqual(got, testCase.want) {
+			t.Errorf("%s:\n got: %v,\n want: %v", testCase.name, got, testCase.want)
 		}
-	})
+	}
 }

3dbd4656f2fa69ca0bf9ebf7b1f39ee24c2ef7d1
Rajivranjan Singh <rajivranjans@mkcl.org>
2022-08-31 10:59:22 +0530

    counting sort added.

--- sorting/linear/countingSort.go
+++ sorting/linear/countingSort.go
@@ -1,17 +1,24 @@
 package linear
 
+// This algorithm is desinged for arrays that contains digits in a pre determined range
 func CountingSort(arr []int, k int) []int {
-	fArr := make([]int, len(arr))
-	tArr := make([]int, k)
+	sortedArr := make([]int, len(arr))
+	tempArr := make([]int, k)
+	// Counting the number of occurence of each element
 	for i := 0; i < len(arr); i++ {
-		tArr[arr[i]-1] += 1
+		tempArr[arr[i]-1] += 1
 	}
+	// Now add the occurence of each element with with count of previous element
+	// This results into each element having a number that represents it's correct index
 	for i := 1; i < k; i++ {
-		tArr[i] += tArr[i-1]
+		tempArr[i] += tempArr[i-1]
 	}
-	for i := len(fArr) - 1; i >= 0; i-- {
-		fArr[tArr[arr[i]-1]-1] = arr[i]
-		tArr[arr[i]-1] -= 1
+	// Now take the index of element from tempArr and place it into correct position in the final array.
+	// Also decrement the index of the same element in tempArr.
+	// Here we scan the orignal array from right to left.
+	for i := len(sortedArr) - 1; i >= 0; i-- {
+		sortedArr[tempArr[arr[i]-1]-1] = arr[i]
+		tempArr[arr[i]-1] -= 1
 	}
-	return fArr
+	return sortedArr
 }
